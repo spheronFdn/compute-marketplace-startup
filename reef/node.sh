1a3106d436b9116abebee2bd2a2b2ed166a6f62a
Manank Patni <manank321@gmail.com>
2022-12-13 23:33:10 +0530

    Add reef node

--- reef/node.sh
+++ reef/node.sh
@@ -2,19 +2,14 @@ echo "Downloading Reef Node Binary..."
 
 curl -s https://api.github.com/repos/reef-defi/reef-chain/releases/latest | grep "/reef-node" | cut -d : -f 2,3 | tr -d \" | wget -qi -
 
+if [ "$ARCHIVE" == "true" ]; then
+  PRUNE="--pruning=archive"
+else
+  PRUNE=""
+fi
+
 chmod +x reef-node
 
 echo "Running Reef Node"
 
-./reef-node \
-  --chain $NETWORK \
-  --base-path /reef/fullnode \
-  --pruning=archive \
-  --port 30333 \
-  --ws-port 9944 \
-  --rpc-port 9933 \
-  --rpc-methods Auto \
-  --rpc-cors all \
-  --rpc-external \
-  --ws-external \
-  --name MyRPCNode
+./reef-node --chain $NETWORK --base-path /reef/fullnode $PRUNE --port 30333 --ws-port 9944 --rpc-port 9933 --rpc-methods Auto --rpc-cors all --rpc-external --ws-external --name MyRPCNode
