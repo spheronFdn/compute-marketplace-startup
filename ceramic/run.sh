3421ac5120ddaa6fe0e609e74fe6744a5383f6e1
Manank Patni <manank321@gmail.com>
2022-12-15 01:01:47 +0530

    Update run.sh

--- ceramic/run.sh
+++ ceramic/run.sh
@@ -2,6 +2,8 @@ echo "Getting config file..."
 
 wget -O daemon.config.json $CONFIG
 
+mkdir $HOME/.ceramic
+
 mv daemon.config.json $HOME/.ceramic/daemon.config.json
 
 echo "Installing the Ceramic CLI..."
