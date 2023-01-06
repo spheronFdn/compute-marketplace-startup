0f340f08e022a8046e3c0b60f75caae0b709c070
Manank Patni <manank321@gmail.com>
2023-01-06 19:47:57 +0530

    Update node.sh

--- sui/node.sh
+++ sui/node.sh
@@ -1,6 +1,6 @@
 echo "Downloading Sui genesis.blob and fullnode.yaml config..."
 
 curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-curl -o fullnode.yaml https://raw.githubusercontent.com/argoapp-live/compute-marketplace-startup/man-jain/sph-587/sui/fullnode-template.yaml
+curl -o fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
 
 /usr/local/bin/sui-node --config-path fullnode.yaml
