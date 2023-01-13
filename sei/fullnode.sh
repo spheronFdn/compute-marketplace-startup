d0cf8f5e449d6e9c1310afeb941f97efb35f2f4b
Manank Patni <manank321@gmail.com>
2023-01-13 18:53:52 +0530

    Update fullnode.sh

--- sei/fullnode.sh
+++ sei/fullnode.sh
@@ -1,10 +1,11 @@
-sudo apt install golang
+sudo apt install golang -y
 git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain
 
 git checkout $(curl -s https://api.github.com/repos/sei-protocol/sei-chain/releases/latest | grep "tag_name" | cut -d : -f 2,3 |  tr -d \" | tr -d ,)
 
 make install
+mv $HOME/go/bin/seid /usr/bin/
 
 seid version --long | head
 
