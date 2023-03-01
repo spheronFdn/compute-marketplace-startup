5f3f8187265866dd6674d6bfd8f2bf67fe8cd196
Manank Patni <manank321@gmail.com>
2023-03-01 19:32:00 +0530

    Update fullnode.sh

--- sei/fullnode.sh
+++ sei/fullnode.sh
@@ -3,7 +3,7 @@ git clone https://github.com/sei-protocol/sei-chain.git
 cd sei-chain
 
 #git checkout $(curl -s https://api.github.com/repos/sei-protocol/sei-chain/releases/latest | grep "tag_name" | cut -d : -f 2,3 |  tr -d \" | tr -d ,)
-git checkout 2.0.36beta
+git checkout $TAG_NAME
 
 make install
 mv $HOME/go/bin/seid /usr/bin/
@@ -12,9 +12,9 @@ seid version --long | head
 
 echo $MONIKER
 
-seid init $MONIKER --chain-id atlantic-2 -o
+seid init $MONIKER --chain-id $CHAIN_ID -o
 
-curl https://raw.githubusercontent.com/sei-protocol/testnet/master/atlantic-2/genesis.json > ~/.sei/config/genesis.json
-curl https://raw.githubusercontent.com/sei-protocol/testnet/master/atlantic-2/addrbook.json > ~/.sei/config/addrbook.json
+curl https://raw.githubusercontent.com/sei-protocol/testnet/master/$CHAIN_ID/genesis.json > ~/.sei/config/genesis.json
+curl https://raw.githubusercontent.com/sei-protocol/testnet/master/$CHAIN_ID/addrbook.json > ~/.sei/config/addrbook.json
 
 seid start
