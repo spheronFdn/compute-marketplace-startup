a61fd90f94efb019f6faec2c39864fb742bba923
Manank Patni <manank321@gmail.com>
2023-11-06 19:36:57 +0530

    Add sui node to bin
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- sui/node.sh
+++ sui/node.sh
@@ -3,4 +3,4 @@ echo "Downloading Sui genesis.blob and fullnode.yaml config..."
 curl -fLJO https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
 curl -o fullnode.yaml https://raw.githubusercontent.com/MystenLabs/sui/main/crates/sui-config/data/fullnode-template.yaml
 
-/usr/local/bin/sui-node --config-path fullnode.yaml
+/opt/sui/bin/sui-node --config-path fullnode.yaml
