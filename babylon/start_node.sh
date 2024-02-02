770d167238317e9e270fbb653b46cdd5f9b2d27b
Mitra <mmitrasish97@gmail.com>
2024-02-03 00:05:13 +0530

    Update config

--- babylon/start_node.sh
+++ babylon/start_node.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Execute setup script to ensure all configurations and directories are properly set up
+/usr/local/bin/setup_node.sh
+
 # Define the full path to cosmovisor
 COSMOVISOR_PATH="/root/go/bin/cosmovisor"
 
