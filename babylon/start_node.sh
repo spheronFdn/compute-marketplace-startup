b673823bd7490ece401d413bf4c991a1507681ff
Mitra <mmitrasish97@gmail.com>
2024-02-03 21:00:42 +0530

    Remove avail bin

--- babylon/start_node.sh
+++ babylon/start_node.sh
@@ -13,3 +13,6 @@ export UNSAFE_SKIP_BACKUP=true
 
 # Start the Babylon node through Cosmovisor using the full path
 exec $COSMOVISOR_PATH run start
+
+
+# babylond tx checkpointing create-validator --amount 1000000ubbn --pubkey $(babylond tendermint show-validator) --moniker "MitraNode" --details "Mitra" --website "https://spheron.network" --chain-id bbn-test-2 --commission-rate 0.05 --commission-max-rate 0.20 --commission-max-change-rate 0.01 --min-self-delegation 1 --from wallet --gas-adjustment 1.4 --gas auto --gas-prices 0.00001ubbn -y
