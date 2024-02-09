e8ee699d03ba03fcaf89aaf567ddbf6b7b394491
Mitrasish Mukherjee <34746546+rekpero@users.noreply.github.com>
2024-02-09 21:16:21 +0530

    Update peering script (#13)

--- fuel/peering-setup.sh
+++ fuel/peering-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the command and capture the output
-OUTPUT=$(fuel-core-keygen new --key-type peering)
+OUTPUT=$(fuel-core-keygen new --key-type peering | tee fuel-key.txt)
 
 # Extract the secret using a tool like jq (assuming the output is JSON)
 SECRET=$(echo $OUTPUT | jq -r '.secret')
