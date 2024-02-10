89200178b54ecc5367fa4ea7d8d852ad70841cc6
Mitra <mmitrasish97@gmail.com>
2024-02-10 21:35:32 +0530

    Add airchain

---
+++ airchain/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initialize Junction Node with MONIKER
+rm -rf ~/.junction
+junctiond init $MONIKER
+
+# Copy the genesis file
+cp /junction/resources/genesis/genesis.json ~/.junction/config/
+
+# Start the Junction Node with PERSISTENT_PEERS
+junctiond start --api.enable --api.address "0.0.0.0:1317" --rpc.laddr "0.0.0.0:26657" --p2p.laddr "0.0.0.0:26656" --p2p.persistent_peers="$PERSISTENT_PEERS" --minimum-gas-prices="0.0001dair"
