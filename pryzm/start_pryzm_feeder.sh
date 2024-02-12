d6dcab390878a30640a78de2d1df46b4cc73cb9b
Mitra <mmitrasish97@gmail.com>
2024-02-12 23:21:13 +0530

    Update pryzm config

--- pryzm/start_pryzm_feeder.sh
+++ pryzm/start_pryzm_feeder.sh
@@ -11,13 +11,17 @@ FEEDER_VALIDATOR="$3"
 
 # Reminder for manual steps
 echo "Please ensure the config.yaml has been correctly updated with your actual details."
+echo $FEEDER_WALLET_ADDRESS
+echo $FEEDER_WALLET_MNEMONIC
+echo $FEEDER_VALIDATOR
+
 # Update config.yaml with sed
 echo "Updating config.yaml with placeholders..."
 sed -i "s/feeder: \".*\"/feeder: \"$FEEDER_WALLET_ADDRESS\"/" /pryzmfeeder/config.yaml
 sed -i "s/feederMnemonic: \".*\"/feederMnemonic: \"$FEEDER_WALLET_MNEMONIC\"/" /pryzmfeeder/config.yaml
 sed -i "s/validator: \".*\"/validator: \"$FEEDER_VALIDATOR\"/" /pryzmfeeder/config.yaml
 
-pryzmd tx oracle delegate-feed-consent $FEEDER_WALLET_ADDRESS --fees 2000factory/pryzm15k9s9p0ar0cx27nayrgk6vmhyec3lj7vkry7rx/uusdsim,3000upryzm --from wallet
+pryzmd tx oracle delegate-feed-consent $FEEDER_WALLET_ADDRESS --fees 2000factory/pryzm15k9s9p0ar0cx27nayrgk6vmhyec3lj7vkry7rx/uusdsim,3000upryzm --from feeder
 
 
-docker run --name=pryzm-feeder --network host --restart=always -v '/pryzmfeeder/config.yaml:/app/config.yaml' europe-docker.pkg.dev/pryzm-zone/core/pryzm-feeder:0.3.4
+docker run --name=pryzm-feeder --network host --restart=always -v '/pryzmfeeder/config.yaml:/app/config.yaml' -v /var/run/docker.sock:/var/run/docker.sock europe-docker.pkg.dev/pryzm-zone/core/pryzm-feeder:0.3.4
