2e10fd482ba8dc44b27a6e723db8ae946dd730bf
Mitra <mmitrasish97@gmail.com>
2024-02-12 21:56:22 +0530

    Update pryzm config

--- pryzm/setup_pryzm_feeder.sh
+++ pryzm/setup_pryzm_feeder.sh
@@ -11,26 +11,32 @@ echo "Installing PostgreSQL..."
 service postgresql start
 
 # PostgreSQL version: Adjust this as necessary
-# PG_VERSION='12' # Example version, adjust based on your actual version
+PG_VERSION='12' # Example version, adjust based on your actual version
 
-# # Path to pg_hba.conf
-# PG_HBA_CONF_PATH="/etc/postgresql/${PG_VERSION}/main/pg_hba.conf"
+# Path to pg_hba.conf
+PG_HBA_CONF_PATH="/etc/postgresql/${PG_VERSION}/main/pg_hba.conf"
 
-# # Modify pg_hba.conf to use md5 for local connections
-# # This sed command targets lines with local connections and changes peer to md5
+# This sed command targets lines with local connections and changes peer to trust
 # sed -i 's/local\s*all\s*postgres\s*peer/local all postgres trust/' "$PG_HBA_CONF_PATH"
-# sed -i 's/local\s*all\s*all\s*peer/local all all trust/' "$PG_HBA_CONF_PATH"
+sed -i 's/local\s*all\s*all\s*peer/local all all trust/' "$PG_HBA_CONF_PATH"
 
-# # Or, if using a service command is possible in your container
-# service postgresql reload
+# Or, if using a service command is possible in your container
+service postgresql reload
 
 # Configure PostgreSQL
-# echo "Configuring PostgreSQL..."
+echo "Configuring PostgreSQL..."
 # Assuming password authentication is configured
-# psql -U postgres -h localhost -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
 # Execute the SQL initialization script
-# psql -U postgres -h localhost -d postgres -f "$HOME/pryzmfeeder/init.sql"
+psql -U postgres -d postgres -f "/pryzmfeeder/init.sql"
+
+# This sed command targets lines with local connections and changes peer to trust
+# sed -i 's/local\s*all\s*postgres\s*peer/local all postgres trust/' "$PG_HBA_CONF_PATH"
+sed -i 's/local\s*all\s*all\s*trust/local all all peer/' "$PG_HBA_CONF_PATH"
+
+# Or, if using a service command is possible in your container
+service postgresql reload
 
 # Reminder to start Pryzm Feeder manually due to Docker usage
 echo "To start Pryzm Feeder, ensure Docker is correctly set up and run the following command in the pryzmfeeder directory:"
--- pryzm/start_pryzm_feeder.sh
+++ pryzm/start_pryzm_feeder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 [your_pryzm_feeder_wallet_address] [your_pryzm_feeder_wallet_mnemonic] [your_pryzm_feeder_validator]"
+    echo "Usage: $0 [your_pryzm_feeder_wallet_address] [your_pryzm_feeder_wallet_mnemonic] [your_pryzm_validator]"
     exit 1
 fi
 
@@ -13,15 +13,11 @@ FEEDER_VALIDATOR="$3"
 echo "Please ensure the config.yaml has been correctly updated with your actual details."
 # Update config.yaml with sed
 echo "Updating config.yaml with placeholders..."
-sed -i "s/feeder: \".*\"/feeder: \"$FEEDER_WALLET_ADDRESS\"/" config.yaml
-sed -i "s/feederMnemonic: \".*\"/feederMnemonic: \"$FEEDER_WALLET_MNEMONIC\"/" config.yaml
-sed -i "s/validator: \".*\"/validator: \"$FEEDER_VALIDATOR\"/" config.yaml
-
-# sed -i 's/feeder: ".*"/feeder: "your_pryzm_feeder_wallet_address"/' config.yaml
-# sed -i 's/feederMnemonic: ".*"/feederMnemonic: "your_pryzm_feeder_mnemonic"/' config.yaml
-# sed -i 's/validator: ".*"/validator: "your_valoper_address"/' config.yaml
+sed -i "s/feeder: \".*\"/feeder: \"$FEEDER_WALLET_ADDRESS\"/" /pryzmfeeder/config.yaml
+sed -i "s/feederMnemonic: \".*\"/feederMnemonic: \"$FEEDER_WALLET_MNEMONIC\"/" /pryzmfeeder/config.yaml
+sed -i "s/validator: \".*\"/validator: \"$FEEDER_VALIDATOR\"/" /pryzmfeeder/config.yaml
 
 pryzmd tx oracle delegate-feed-consent $FEEDER_WALLET_ADDRESS --fees 2000factory/pryzm15k9s9p0ar0cx27nayrgk6vmhyec3lj7vkry7rx/uusdsim,3000upryzm --from wallet
 
 
-docker run --name=pryzm-feeder --network host --restart=always -v '$(pwd)/config.yaml:/app/config.yaml' -v '$(pwd)/logs:/app/logs' europe-docker.pkg.dev/pryzm-zone/core/pryzm-feeder:0.3.4
+docker run --name=pryzm-feeder --network host --restart=always -v '/pryzmfeeder/config.yaml:/app/config.yaml' europe-docker.pkg.dev/pryzm-zone/core/pryzm-feeder:0.3.4
