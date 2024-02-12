7e2f6e7b5fc3a92ea1b97c3b4926261d8fdba5e4
Mitra <mmitrasish97@gmail.com>
2024-02-12 19:56:04 +0530

    Update pryzm config

--- pryzm/setup_pryzm_feeder.sh
+++ pryzm/setup_pryzm_feeder.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 
-# Reminder to set up the Pryzm Feeder wallet manually.
-echo "Remember to set up the Pryzm Feeder wallet manually."
-# pryzmd keys add feeder
-# OR
-# pryzmd keys add feeder --recover
-
 # Download Pryzm Feeder configuration
 echo "Downloading Pryzm Feeder configuration..."
 mkdir -p /pryzmfeeder && cd /pryzmfeeder
 wget https://storage.googleapis.com/pryzm-zone/feeder/config.yaml https://storage.googleapis.com/pryzm-zone/feeder/init.sql https://storage.googleapis.com/pryzm-zone/feeder/docker-compose.yml
 
-# Update config.yaml with sed
-# echo "Updating config.yaml with placeholders..."
-# sed -i 's/feeder: ".*"/feeder: "your_pryzm_feeder_wallet_address"/' config.yaml
-# sed -i 's/feederMnemonic: ".*"/feederMnemonic: "your_pryzm_feeder_mnemonic"/' config.yaml
-# sed -i 's/validator: ".*"/validator: "your_valoper_address"/' config.yaml
 
 # Install PostgreSQL (optional, based on your Docker container setup)
 echo "Installing PostgreSQL..."
-apt-get update && apt-get install -y postgresql postgresql-contrib
 service postgresql start
 
 # Configure PostgreSQL
 echo "Configuring PostgreSQL..."
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
-psql -U postgres -h localhost -W -c "\i /pryzmfeeder/init.sql"
+psql -U postgres -h localhost -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+psql -U postgres -h localhost -c "\i /pryzmfeeder/init.sql"
 
-# Reminder for manual steps
-echo "Please ensure the config.yaml has been correctly updated with your actual details."
-echo "Remember to initiate oracle delegate feed consent tx manually."
 
 # Reminder to start Pryzm Feeder manually due to Docker usage
 echo "To start Pryzm Feeder, ensure Docker is correctly set up and run the following command in the pryzmfeeder directory:"
+
+# pryzmd keys add feeder
+# pryzmd keys show wallet --bech val -a
