cdc40778c421829fccbbc5e0d58528d1e9641ada
Mitra <mmitrasish97@gmail.com>
2024-02-25 23:05:51 -0700

    Update babylon node

--- babylon/setup_node.sh
+++ babylon/setup_node.sh
@@ -7,30 +7,35 @@ MONIKER=${MONIKER:-"YOUR_MONIKER_GOES_HERE"}
 babylond init "$MONIKER" --chain-id bbn-test-3
 
 # Download genesis and addrbook
-wget https://github.com/babylonchain/networks/raw/main/bbn-test-3/genesis.tar.bz2
-tar -xjf genesis.tar.bz2 && rm genesis.tar.bz2
-mv genesis.json ~/.babylond/config/genesis.json
+wget -O genesis.json https://snapshots.polkachu.com/testnet-genesis/babylon/genesis.json --inet4-only
+mv genesis.json ~/.babylond/config
+
+wget -O addrbook.json https://snapshots.polkachu.com/testnet-addrbook/babylon/addrbook.json --inet4-only
+mv addrbook.json ~/.babylond/config
 
 # Add seeds
-sed -i -e "s|^seeds *=.*|seeds = \"49b4685f16670e784a0fe78f37cd37d56c7aff0e@3.14.89.82:26656,9cb1974618ddd541c9a4f4562b842b96ffaf1446@3.16.63.237:26656\"|" $HOME/.babylond/config/config.toml
+# PEERS=e8f550ed3fea54eda7fa3f8ed3d6b17cb222fedf@95.111.239.100:26656,82191d0763999d30e3ddf96cc366b78694d8cee1@162.19.169.211:26656,49b15e202497c231ebe7b2a56bb46cfc60eff78c@135.181.134.151:46656,c0ee3e7f140b2de189ce853cfccb9fb2d922eb66@95.217.203.226:26656,4dbf5157b735de59fb84be26f2bd40a16cee056c@54.238.212.246:26656
+# sed -i -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.babylond/config/config.toml
+
+sed -i -e "s|^seeds *=.*|seeds = \"ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:20656\"|" $HOME/.babylond/config/config.toml
 
 # Set minimum gas price
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.00001ubbn\"|" $HOME/.babylond/config/app.toml
 
 # Switch to signet
-sed -i -e "s|^network *=.*|network = \"signet\"|" $HOME/.babylond/config/app.toml
+# sed -i -e "s|^network *=.*|network = \"signet\"|" $HOME/.babylond/config/app.toml
 
 # Set pruning
 sed -i \
   -e 's|^pruning *=.*|pruning = "custom"|' \
   -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
   -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "10"|' \
   $HOME/.babylond/config/app.toml
 
 # Download latest chain snapshot
-# curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
-# [[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
+curl -L https://snapshots.polkachu.com/testnet-snapshots/babylon/babylon_44206.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
+[[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
 
 # Start the node (if applicable, or configure to start with Docker container)
 # This part may need to be handled outside of the script for Docker deployments
