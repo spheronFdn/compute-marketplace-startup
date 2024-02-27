2cfada5ba78bae95833d442104552d0129162cf7
Mitra <mmitrasish97@gmail.com>
2024-02-27 09:28:53 -0700

    Update babylon

--- babylon/setup_node.sh
+++ babylon/setup_node.sh
@@ -7,37 +7,32 @@ MONIKER=${MONIKER:-"YOUR_MONIKER_GOES_HERE"}
 babylond init "$MONIKER" --chain-id bbn-test-3
 
 # Download genesis and addrbook
-wget -O genesis.json https://snapshots.polkachu.com/testnet-genesis/babylon/genesis.json --inet4-only
-mv genesis.json ~/.babylond/config
-
-wget -O addrbook.json https://snapshots.polkachu.com/testnet-addrbook/babylon/addrbook.json --inet4-only
-mv addrbook.json ~/.babylond/config
+curl -Ls https://snapshots.kjnodes.com/babylon-testnet/genesis.json > $HOME/.babylond/config/genesis.json
+curl -Ls https://snapshots.kjnodes.com/babylon-testnet/addrbook.json > $HOME/.babylond/config/addrbook.json
 
 # Add seeds
-PEERS=69c1b7e1eb114703733c3000baa6c008ebc70073@65.109.113.233:20656,6990fd085c9e2e8c9256f144799d18df51f74022@141.94.195.144:26656,49b15e202497c231ebe7b2a56bb46cfc60eff78c@135.181.134.151:46656,ce1caddb401d530cc2039b219de07994fc333dcf@162.19.97.200:26656,26cb133489436035829b6920e89105046eccc841@178.63.95.125:26656,25abb614b96fa606fb5514fcf711635e8e861d8f@217.72.207.107:26656
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.babylond/config/config.toml
-
-sed -i -e "s|^seeds *=.*|seeds = \"ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:20656\"|" $HOME/.babylond/config/config.toml
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@babylon-testnet.rpc.kjnodes.com:16459\"|" $HOME/.babylond/config/config.toml
 
 # Set minimum gas price
 sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.00001ubbn\"|" $HOME/.babylond/config/app.toml
-
-# Switch to signet
-# sed -i -e "s|^network *=.*|network = \"signet\"|" $HOME/.babylond/config/app.toml
 
 # Set pruning
 sed -i \
   -e 's|^pruning *=.*|pruning = "custom"|' \
   -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
   -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "10"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
   $HOME/.babylond/config/app.toml
+
+# Set custom ports
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:16458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:16457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:16460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:16456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":16466\"%" $HOME/.babylond/config/config.toml
+sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:16417\"%; s%^address = \":8080\"%address = \":16480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:16490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:16491\"%; s%:8545%:16445%; s%:8546%:16446%; s%:6065%:16465%" $HOME/.babylond/config/app.toml
 
 # Download latest chain snapshot
 # curl -L https://snapshots.polkachu.com/testnet-snapshots/babylon/babylon_44206.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
 # [[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
 
-wget -O babylon_47173.tar.lz4 https://snapshots.polkachu.com/testnet-snapshots/babylon/babylon_47173.tar.lz4 --inet4-only
+wget -O babylon_50148.tar.lz4 https://snapshots.polkachu.com/testnet-snapshots/babylon/babylon_50148.tar.lz4 --inet4-only
 lz4 -c -d babylon_47173.tar.lz4  | tar -x -C $HOME/.babylond
 # Start the node (if applicable, or configure to start with Docker container)
 # This part may need to be handled outside of the script for Docker deployments
