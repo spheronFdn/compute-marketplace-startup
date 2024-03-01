09b42b69657aa751809bfb3f9de1880dae487f0f
Mitra <mmitrasish97@gmail.com>
2024-03-01 09:28:01 -0700

    Update dusk

--- babylon/setup_node.sh
+++ babylon/setup_node.sh
@@ -32,15 +32,5 @@ sed -i \
   -e 's|^pruning-interval *=.*|pruning-interval = "10"|' \
   $HOME/.babylond/config/app.toml
 
-# Set custom ports
-# sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:16458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:16457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:16460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:16456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":16466\"%" $HOME/.babylond/config/config.toml
-# sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:16417\"%; s%^address = \":8080\"%address = \":16480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:16490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:16491\"%; s%:8545%:16445%; s%:8546%:16446%; s%:6065%:16465%" $HOME/.babylond/config/app.toml
-
-# Download latest chain snapshot
-# curl -L https://snapshots.kjnodes.com/babylon-testnet/snapshot_latest.tar.lz4 | tar -Ilz4 -xf - -C $HOME/.babylond
-# [[ -f $HOME/.babylond/data/upgrade-info.json ]] && cp $HOME/.babylond/data/upgrade-info.json $HOME/.babylond/cosmovisor/genesis/upgrade-info.json
-
-# Start the node (if applicable, or configure to start with Docker container)
-# This part may need to be handled outside of the script for Docker deployments
 
 echo "Setup complete. Node configured and ready."
