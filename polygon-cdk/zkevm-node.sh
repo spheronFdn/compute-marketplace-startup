cdc22f862d2e85536e62e30e6f080bc025eb4297
Mitra <mmitrasish97@gmail.com>
2024-03-25 17:55:29 +0530

    Update bash script

--- polygon-cdk/zkevm-node.sh
+++ polygon-cdk/zkevm-node.sh
@@ -2,7 +2,7 @@
 
 # Set variables
 DEPENDENCIES="unzip" # Add other dependencies separated by spaces
-URL="https://github.com/spheronFdn/compute-marketplace-startup/raw/rekpero/polygon-cdk/polygon-cdk/zkevm-node.zip"
+URL="https://bafybeigjhwufcgrg4fdffu65wcdhshn2vuh3dpeqkg6ewk2m5tx5rcuh6q.ipfs.sphn.link/zkevm-node.zip"
 APP_DIR="/app"
 PK_DIR="/pk"
 ZIP_FILE="zkevm-node.zip"
@@ -23,8 +23,8 @@ unzip $ZIP_FILE -d $TEMP_DIR
 
 # Move the files
 echo "Moving files to the destination directory..."
-mv $TEMP_DIR/app/* $APP_DIR
-mv $TEMP_DIR/pk/* $PK_DIR
+mv $TEMP_DIR/zkevm-node/app/* $APP_DIR
+mv $TEMP_DIR/zkevm-node/pk/* $PK_DIR
 
 # Clean up
 echo "Cleaning up..."
